333113a1f74440bb566a9c1e210f168b3516df7b
Lee Brooks <lee@kagiso.io>
2016-02-18 09:24:06 +0200

    Moved sitemap location

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='kagiso_sitemap',
-    version='0.0.6',
+    version='0.0.7',
     author='Kagiso Media',
     author_email='development@kagiso.io',
     description='Kagiso Sitemap',
